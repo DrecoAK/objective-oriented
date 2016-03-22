786fe11cdd2abc2e2753531b9013c49a669e5fbe
dreco <dreco@192.168.25.10>
2016-03-21 23:25:00 -0300

    Versão Reestruturada Para o formato proposto no problema!

--- SistemasInteligentes1/src/sistemasinteligentes1/Agente.java
+++ SistemasInteligentes1/src/sistemasinteligentes1/Agente.java
@@ -8,55 +8,36 @@ import java.util.ArrayList;
  */
 public class Agente {
     
-    private String[] q;
-    // private float[][] pesoCaminhos; // talvez
-    private String qf;
-    private String qi;
-    
+    private Estado qi, qf, qa;
     private EspacoEstados es;
 
-    public Agente()
+    public Agente(EspacoEstados es)
     {
+        this.es = es;
     }
     
-    public void capturar()
+    public void perceber(EspacoEstados es, Estado qi,Estado qf)
     {
-        // se o cálculo é simulado, o agente ainda precisa fazer a busca?
-        // em todo caso, esse é o código de criação do ambiente
-        // cria a parada toda e coloca tudo no espaço de estados
-        
-        Estado p = new Estado("Portal da Graciosa");
-        Estado s = new Estado("São João");
-        Estado b = new Estado("Bufara");
-        Estado m = new Estado("Morretes");
-        Estado a = new Estado("Antonina");
-        
-        Aresta ps = new Aresta(p, s, 19.1, 18, true);
-        Aresta sb = new Aresta(s, b, 17.7, 18, true);
-        Aresta sm = new Aresta(s, m, 13.2, 14, true);
-        Aresta mb = new Aresta(m, b, 7.0, 8, true);
-        Aresta ba = new Aresta(b, a, 6.9, 8, true);
-        
-        ArrayList<Estado> estados = new ArrayList<Estado>();
-        estados.add(p);
-        estados.add(s);
-        estados.add(b);
-        estados.add(m);
-        estados.add(a);
-        
-        ArrayList<Aresta> arestas = new ArrayList<Aresta>();
-        arestas.add(ps);
-        arestas.add(sb);
-        arestas.add(sm);
-        arestas.add(mb);
-        arestas.add(ba);
-        
-        es = new EspacoEstados(estados, arestas);
+        this.es = es;
+        this.qi = qi;
+        this.qf = qf;
+    }
+   
+    public void perceber(Estado qi, Estado qf)
+    {
+        this.qi = qi;
+        this.qf = qf;
+    }
+    
+    public void perceber(EspacoEstados es)
+    {
+        this.es = es;
     }
     
     // escolha da próxima ação
     public void deliberar()
     {
+        
     }
     
     // executar ação no ambiente (em resposta a uma ação, o ambiente deve mudar de estado)
--- SistemasInteligentes1/src/sistemasinteligentes1/Main.java
+++ SistemasInteligentes1/src/sistemasinteligentes1/Main.java
@@ -1,5 +1,7 @@
 package sistemasinteligentes1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author André, Jorge
@@ -8,5 +10,39 @@ public class Main
 {
     public static void main(String[] args)
     {
+        
+        Estado p = new Estado("Portal da Graciosa");
+        Estado s = new Estado("São João");
+        Estado b = new Estado("Bufara");
+        Estado m = new Estado("Morretes");
+        Estado a = new Estado("Antonina");
+        
+        Aresta ps = new Aresta(p, s, 19.1, 18, true);
+        Aresta sb = new Aresta(s, b, 17.7, 18, true);
+        Aresta sm = new Aresta(s, m, 13.2, 14, true);
+        Aresta mb = new Aresta(m, b, 7.0, 8, true);
+        Aresta ba = new Aresta(b, a, 6.9, 8, true);
+        
+        ArrayList<Estado> estados = new ArrayList<Estado>();
+        estados.add(p);
+        estados.add(s);
+        estados.add(b);
+        estados.add(m);
+        estados.add(a);
+        
+        ArrayList<Aresta> arestas = new ArrayList<Aresta>();
+        arestas.add(ps);
+        arestas.add(sb);
+        arestas.add(sm);
+        arestas.add(mb);
+        arestas.add(ba);
+        
+        EspacoEstados es = new EspacoEstados(estados, arestas);
+        
+        Agente agente = new Agente(es);
+        
+        agente.perceber(p, a);
+        
+        agente.deliberar();
     }
 }
